2e15b7d54aca8eefcbf56a9882bd33b4720b3476
siberianworker <siberianworkshop24@gmail.com>
2023-03-16 17:39:43 +0700

    правки

--- handlers/client.py
+++ handlers/client.py
@@ -5,35 +5,42 @@ from create_bot import dp, bot
 from keyboards import client_kb
 from information import base
 
-zero = ''
-one = 'AgACAgIAAxkBAAMhZBCgyEqr6sgPYWAaQcnII0O2mNMAAtHGMRtdjolIAqZh6sTIcecBAAMCAAN5AAMvBA'
+#Тестовые
+#one = 'AgACAgIAAxkBAAMhZBCgyEqr6sgPYWAaQcnII0O2mNMAAtHGMRtdjolIAqZh6sTIcecBAAMCAAN5AAMvBA'
 two = 'AgACAgIAAxkBAAMrZBFpUnVjXGCwT_fCWqIfKuvMLZcAAnXDMRtdjpFIP2DSB_mtWt8BAAMCAAN5AAMvBA'
 three = 'AgACAgIAAxkBAAMtZBFpZXyc0W-UhG8p4hdyGsTYQa4AAnfDMRtdjpFIYjwY9EpRxrwBAAMCAAN5AAMvBA'
-
-file0 = InputMediaPhoto(zero, caption=base.welcome)
-file1 = InputMediaPhoto(one, caption=base.run)
+#file1 = InputMediaPhoto(one, caption=base.run)
 file2 = InputMediaPhoto(two, caption=base.run1)
 file3 = InputMediaPhoto(three, caption=base.run2)
 
+#Скачать шаблон
+#a1 = 'AgACAgIAAxkBAAOJZBG8JlY8Lf_mzdIvJed1A18yj8UAAkDFMRtdjpFI-cEbWykYWaEBAAMCAAN4AAMvBA'
+a1 = 'AgACAgIAAxkBAAOLZBG8UHDAENxv4hmz_EkEVg4xOncAAkLFMRtdjpFI6oXhDFP3l0YBAAMCAAN5AAMvBA'
+#media_group1 = [a1, a2]
+
+
+#Команда /start
 async def command_start(message : types.Message):
     await bot.send_message(message.from_user.id, base.welcome, reply_markup=client_kb.ika)
 
 
 @dp.callback_query_handler()
 async def first_call(callback: types.CallbackQuery):
-
+    #Возврат в начало
     if callback.data == 'start':
         await callback.message.delete()
         await callback.message.answer(text=base.welcome, reply_markup=client_kb.ika)
     elif callback.data == 'run':
-        await callback.message.answer_photo(one, base.run, reply_markup=client_kb.ikb)
+        await callback.message.answer_photo(a1, base.run, reply_markup=client_kb.ikb)
         await callback.message.delete()
     elif callback.data == 'run1':
-        await callback.message.edit_media(file2, reply_markup=client_kb.ikc)
+#        await callback.message.edit_media(file2, reply_markup=client_kb.ikc)
+        await callback.message.answer(text=base.run1, reply_markup=client_kb.ikc)
+        await callback.message.delete()
     elif callback.data == 'run2':
         await callback.message.edit_media(file3, reply_markup=client_kb.ikd)
-    elif callback.data == 'back':
-       await callback.message.edit_media(file1, reply_markup=client_kb.ikb)
+#    elif callback.data == 'back':
+#       await callback.message.edit_media(file1, reply_markup=client_kb.ikb)
     elif callback.data == 'back1':
         await callback.message.edit_media(file2, reply_markup=client_kb.ikc)
 
--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,6 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-home = InlineKeyboardButton(text='С начала', callback_data='start')
+home = InlineKeyboardButton(text='С начала 🔝', callback_data='start')
 back = InlineKeyboardButton(text='Назад 🔙', callback_data='back')
 back1 = InlineKeyboardButton(text='Назад 🔙', callback_data='back1')
 
@@ -11,12 +11,15 @@ ika.add(ib)
 
 #Вторая
 ikb = InlineKeyboardMarkup(row_width=2)
-button1 = InlineKeyboardButton(text='Продолжить', callback_data='run1')
-ikb.add(button1).add(home)
+next1 = InlineKeyboardButton(text='Продолжить ✅', callback_data='run1')
+shablon1 = InlineKeyboardButton(text='Шаблон для доступов', url='https://docs.google.com/spreadsheets/d/1YQqEAYWyIrRUB2jr9dKVZr_x3Mz7qHNN3pBN1I_U_qQ/edit#gid=0')
+ikb.add(next1, home).add(shablon1)
 
 ikc = InlineKeyboardMarkup(row_width=2)
-button1 = InlineKeyboardButton(text='Продолжить', callback_data='run2')
-ikc.add(button1).add(home, back)
+#next2 = InlineKeyboardButton(text='Продолжить ✅', callback_data='run2')
+pc = InlineKeyboardButton(text='На свой ПК', callback_data='pc')
+
+ikc.add(pc).add(home, back)
 
 ikd = InlineKeyboardMarkup(row_width=2)
 ikd.add(home, back1)
